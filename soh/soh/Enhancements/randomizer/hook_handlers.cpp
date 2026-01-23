ed452e9e34c8dc79ff90c7e1a3cf12cacd198f2f
ItsHeckinPat <Patrick12115@hotmail.com>
2026-01-23 01:34:45 -0500

    Add fix for skipped checks and skip warp song cutscene with misc skip

--- soh/soh/Enhancements/randomizer/hook_handlers.cpp
+++ soh/soh/Enhancements/randomizer/hook_handlers.cpp
@@ -2471,7 +2471,7 @@ void RandomizerOnActorUpdateHandler(void* refActor) {
     }
 
     // In ER, override the warp song locations. Also removes the warp song cutscene
-    if (RAND_GET_OPTION(RSK_SHUFFLE_ENTRANCES) && actor->id == ACTOR_DEMO_KANKYO &&
+    if ((RAND_GET_OPTION(RSK_SHUFFLE_ENTRANCES) || (CVarGetInteger(CVAR_ENHANCEMENT("TimeSavers.SkipMiscInteractions"), 0))) && actor->id == ACTOR_DEMO_KANKYO &&
         actor->params == 0x000F) { // Warp Song particles
         Entrance_SetWarpSongEntrance();
     }
